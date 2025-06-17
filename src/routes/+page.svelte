ae615c68926eed0bd60c9825e657074e80b66ebc
TDkRecords <tdksongs@gmail.com>
2025-06-17 08:52:21 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,78 @@
+<script>
+    import danna from "$lib/images/danna.jpg";
+</script>
+
+<img src={danna} alt="danna.jpg" width="144px" />
+<h1>Danna Gabriela Betancourt Chamorro</h1>
+<code>Chambeadora pro</code>
+<br />
+<h3>perfil</h3>
+<p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum debitis,
+    voluptates id aperiam nisi dignissimos deleniti? Quis, ipsa omnis.
+    Praesentium doloremque, quam adipisci hic architecto aliquam rerum sequi,
+    explicabo a molestiae dolorem perferendis sit dolores facere aperiam, eum
+    voluptates quo? Ipsa numquam facilis aperiam praesentium quis explicabo
+    earum animi quidem ratione corrupti? Nobis incidunt praesentium error animi
+    consectetur necessitatibus sit soluta cum voluptatem quis laboriosam
+    accusamus iure atque corporis vero, ratione voluptates fuga cupiditate
+    quaerat id labore ex veritatis asperiores. Facilis, earum placeat? Saepe
+    deserunt quam, voluptate corrupti itaque inventore facere explicabo dolor
+    sapiente dolore numquam ratione voluptatem doloribus, aspernatur rem magnam
+    voluptates veritatis illo odit perspiciatis nam eaque doloremque! Reiciendis
+    in sequi exercitationem! Labore autem mollitia dolores ut reprehenderit
+    cupiditate quasi quia modi! Officia temporibus est quis, consequuntur
+    voluptatibus animi inventore harum suscipit autem similique corporis atque
+    maiores laborum, provident debitis, error hic in? Sed quo nemo quibusdam
+    minus.
+</p>
+<br />
+<h3>Experiencia Laboral</h3>
+<p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, harum
+    vitae! Debitis qui eaque deserunt maxime repellendus architecto minus cum
+    similique reprehenderit expedita consectetur ipsa unde facere corporis
+    facilis, nemo, officia commodi quam illum corrupti! Necessitatibus
+    consequuntur voluptatum, quis velit est delectus obcaecati? Vitae adipisci
+    aliquid nisi nemo quo tenetur eaque, molestias dolores corporis. Velit
+    placeat recusandae perferendis ex quaerat voluptatum deserunt, laboriosam
+    tempora praesentium quasi nobis incidunt expedita. Inventore dolorum
+    expedita cumque aliquam asperiores accusantium maiores!
+</p>
+<p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod in fuga
+    repellendus tempora recusandae cum eum accusantium, doloremque consectetur
+    officia minima iusto deserunt aliquid ea minus pariatur ad nihil voluptate
+    quae nostrum? Recusandae aspernatur iste neque reprehenderit unde excepturi
+    enim nostrum tempora, ut laborum! Exercitationem ipsam culpa dolore fugiat
+    consequatur, pariatur sequi aut voluptas ipsa, magni nemo totam odit at!
+    Eius quia aspernatur maiores quisquam, obcaecati corrupti perferendis
+    voluptatibus numquam architecto eveniet consectetur perspiciatis ipsa
+    incidunt illum.
+</p>
+<p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit nisi aliquam
+    labore magnam odio sed natus obcaecati vel deserunt itaque. Nemo aspernatur
+    nesciunt dicta ab nulla illo eveniet sit esse. Libero ullam nobis minima
+    repellat rerum nemo quo voluptate repudiandae laudantium, optio deleniti.
+    Quisquam, debitis id!
+</p>
+
+<br />
+<h3>estudios</h3>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt dolores
+    quaerat alias quod voluptate ab error eligendi!
+</p>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt dolores
+    quaerat alias quod voluptate ab error eligendi!
+</p>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt dolores
+    quaerat alias quod voluptate ab error eligendi!
+</p>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt dolores
+    quaerat alias quod voluptate ab error eligendi!
+</p>
